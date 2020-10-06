8927c13c3a6332296150ee56b53cf2bf39a183ce
Carlos H <djtv93@yahoo.es>
2020-10-06 20:33:46 +0200

    created boards.c and calculate_EQ()

---
+++ src/boards.c
@@ -0,0 +1,253 @@
+#include "header.h"
+#include "conv_pkr.h"
+#include "get_value_combo.h"
+
+//gcc boards.c -lpcre
+void main(){
+    init_vars();
+    int i=0;
+    int j;
+    board[0]=0;//h
+    board[1]=9;//A
+    board[2]=2;//h
+    board[3]=0xb;//5
+    board[4]=2;//s
+    board[5]=6;//4
+//     board[6]=1;//d
+//     board[7]=8;//8
+
+    int *arr_hex_pos0,*arr_hex_pos1;
+    
+    int long_hex_pos0=0;
+    int long_hex_pos1=0;
+    //  char range_pkr[]={"AsAc, A2s, Kx,A2s-A9s,33-66,99"};
+    //  char range_pkr[]={"88"};   
+    
+    char *range_pkr1="AQs-A3s";
+    char *range_pkr2="AA-55";
+//     printf("%s",range_pkr1);
+    if(range_pkr1){
+        //the result of this function will be saved in arr_hex_return
+        conv_range_pkr_to_hex(range_pkr1);
+        i=0;
+        while(*(arr_hex_return+i)){
+            i++;
+        }
+        long_hex_pos0=i;
+        arr_hex_pos0=malloc(sizeof(int)*long_hex_pos0);//create the space for save arr_hex_return
+        memcpy((void *)arr_hex_pos0,arr_hex_return,sizeof(int)*long_hex_pos0);
+
+    }
+
+    if(range_pkr2){
+        //the result of this function will be saved in arr_hex_return
+        conv_range_pkr_to_hex(range_pkr2);
+        i=0;
+        while(*(arr_hex_return+i)){
+            i++;
+        }
+        long_hex_pos1=i;
+        arr_hex_pos1=malloc(sizeof(int)*long_hex_pos1);//create the space for save arr_hex_return
+        memcpy((void *)arr_hex_pos1,arr_hex_return,sizeof(int)*long_hex_pos1);
+    }
+
+//     for(i=0;i<long_hex_pos0;i++){
+//         printf("pasado i1 %d hero combos %x\n",i,arr_hex_pos0[i]);
+//     }
+//     for(i=0;i<long_hex_pos1;i++){
+//         printf("pasado i2 %d hero combos %x\n",i,arr_hex_pos1[i]);
+//     } 
+// //     
+//     
+//     
+    calculate_EQ(arr_hex_pos0,long_hex_pos0,arr_hex_pos1,long_hex_pos1);
+    free(arr_hex_pos0);
+    free(arr_hex_pos1);
+}
+
+void calculate_EQ(int *arr_hex_pos0,int long_hex_pos0, int *arr_hex_pos1,int long_hex_pos1){
+    char i,j,k,l,m,n;
+    int local_board[10];
+    int combo_hex[2][4];
+
+    memcpy(local_board, board, 10*sizeof(int));
+    int tot_combos=0;//total combos analized
+    float tot_win_pos[9]={};//save in each index the result when this position have the highest score
+    int tmp_val[9]={};//save here the values for compare
+    float tot_win_pcent[9];
+    if(local_board[1]==0){
+        //all the board is empty
+    }else if(local_board[6]==0 && local_board[7]==0){//turn is empty
+        for(i=0;i<4;i++){
+            for(j=2;j<=14;j++){
+                for(k=0;k<4;k++){
+                    for(l=2;l<=14;l++){
+                        if( !(i==k && j==l) &&
+                            !(board[0]==i && board[1]==j) &&
+                            !(board[2]==i && board[3]==j) &&
+                            !(board[4]==i && board[5]==j) &&
+                            
+                            !(board[0]==k && board[1]==l) &&
+                            !(board[2]==k && board[3]==l) &&
+                            !(board[4]==k && board[5]==l)
+                        ){
+                            local_board[6]=i;
+                            local_board[7]=j;
+                            local_board[8]=k;
+                            local_board[9]=l;
+                            
+                            for(m=0;m<long_hex_pos0;m++){
+                                for(n=0;n<long_hex_pos1;n++){
+                                    
+                                    combo_hex[0][0]=(*(arr_hex_pos0+m)/0x1000)%0x10;
+                                    combo_hex[0][1]=(*(arr_hex_pos0+m)/0x100)%0x10;
+                                    combo_hex[0][2]=(*(arr_hex_pos0+m)/0x10)%0x10;
+                                    combo_hex[0][3]=*(arr_hex_pos0+m)%0x10;
+
+
+                                    combo_hex[1][0]=(*(arr_hex_pos1+m)/0x1000)%0x10;
+                                    combo_hex[1][1]=(*(arr_hex_pos1+m)/0x100)%0x10;
+                                    combo_hex[1][2]=(*(arr_hex_pos1+m)/0x10)%0x10;
+                                    combo_hex[1][3]=*(arr_hex_pos1+m)%0x10;
+
+                                    if(
+                                        !(combo_hex[0][0]==board[0] && combo_hex[0][1]==board[1]) &&
+                                        !(combo_hex[0][2]==board[0] && combo_hex[0][3]==board[1]) &&
+                                        !(combo_hex[0][0]==board[2] && combo_hex[0][1]==board[3]) &&
+                                        !(combo_hex[0][2]==board[2] && combo_hex[0][3]==board[3]) &&
+                                        !(combo_hex[0][0]==board[4] && combo_hex[0][1]==board[5]) &&
+                                        !(combo_hex[0][2]==board[4] && combo_hex[0][3]==board[5]) &&
+                                        !(combo_hex[0][0]==local_board[6] && combo_hex[0][1]==local_board[7]) &&
+                                        !(combo_hex[0][2]==local_board[6] && combo_hex[0][3]==local_board[7]) &&
+                                        !(combo_hex[0][0]==local_board[8] && combo_hex[0][1]==local_board[9]) &&
+                                        !(combo_hex[0][2]==local_board[8] && combo_hex[0][3]==local_board[9]) &&
+                                
+                                        !(combo_hex[1][0]==board[0] && combo_hex[1][1]==board[1]) &&
+                                        !(combo_hex[1][2]==board[0] && combo_hex[1][3]==board[1]) &&
+                                        !(combo_hex[1][0]==board[2] && combo_hex[1][1]==board[3]) &&
+                                        !(combo_hex[1][2]==board[2] && combo_hex[1][3]==board[3]) &&
+                                        !(combo_hex[1][0]==board[4] && combo_hex[1][1]==board[5]) &&
+                                        !(combo_hex[1][2]==board[4] && combo_hex[1][3]==board[5]) &&
+                                        !(combo_hex[1][0]==local_board[6] && combo_hex[1][1]==local_board[7]) &&
+                                        !(combo_hex[1][2]==local_board[6] && combo_hex[1][3]==local_board[7]) &&
+                                        !(combo_hex[1][0]==local_board[8] && combo_hex[1][1]==local_board[9]) &&
+                                        !(combo_hex[1][2]==local_board[8] && combo_hex[1][3]==local_board[9])
+                                        
+                                    ){
+                                        tmp_val[0]=get_value_combo(arr_hex_pos0[m],local_board);
+                                        tmp_val[1]=get_value_combo(arr_hex_pos1[n],local_board);
+
+                                        if(tmp_val[0]>tmp_val[1]){
+                                            tot_win_pos[0]++;
+                                        }else if(tmp_val[0]<tmp_val[1]){
+                                            tot_win_pos[1]++;                                        
+                                        }
+//                                         tot_combos++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        tot_combos=tot_win_pos[0]+tot_win_pos[1];
+        tot_win_pcent[0]=(tot_win_pos[0]*100)/tot_combos;
+        tot_win_pcent[1]=(tot_win_pos[1]*100)/tot_combos;
+
+        printf("SIN TURN Y RIVER pos[0] %d\n",(int) tot_win_pos[0]);
+        printf("SIN TURN Y RIVER pos[1] %d\n",(int) tot_win_pos[1]);
+
+        printf("tot_win_pcent[0] %.2f\n",tot_win_pcent[0]);
+        printf("tot_win_pcent[1] %.2f\n",tot_win_pcent[1]);
+        
+        printf("tot_combos %d\n",tot_combos);
+
+        
+    }else if(local_board[8]==0 && local_board[9]==0){//river is empty
+        for(k=0;k<4;k++){
+            for(l=2;l<=14;l++){
+
+                if(
+                    !(board[0]==k && board[1]==l) &&
+                    !(board[2]==k && board[3]==l) &&
+                    !(board[4]==k && board[5]==l) &&
+                    !(board[6]==k && board[7]==l)
+                ){
+                    local_board[8]=k;
+                    local_board[9]=l;
+
+                    for(m=0;m<long_hex_pos0;m++){
+                        for(n=0;n<long_hex_pos1;n++){
+                            combo_hex[0][0]=(*(arr_hex_pos0+m)/0x1000)%0x10;
+                            combo_hex[0][1]=(*(arr_hex_pos0+m)/0x100)%0x10;
+                            combo_hex[0][2]=(*(arr_hex_pos0+m)/0x10)%0x10;
+                            combo_hex[0][3]=*(arr_hex_pos0+m)%0x10;
+
+                            combo_hex[1][0]=(*(arr_hex_pos1+m)/0x1000)%0x10;
+                            combo_hex[1][1]=(*(arr_hex_pos1+m)/0x100)%0x10;
+                            combo_hex[1][2]=(*(arr_hex_pos1+m)/0x10)%0x10;
+                            combo_hex[1][3]=*(arr_hex_pos1+m)%0x10;
+
+                            if(                                
+                                !(combo_hex[0][0]==board[0] && combo_hex[0][1]==board[1]) &&
+                                !(combo_hex[0][2]==board[0] && combo_hex[0][3]==board[1]) &&
+                                !(combo_hex[0][0]==board[2] && combo_hex[0][1]==board[3]) &&
+                                !(combo_hex[0][2]==board[2] && combo_hex[0][3]==board[3]) &&
+                                !(combo_hex[0][0]==board[4] && combo_hex[0][1]==board[5]) &&
+                                !(combo_hex[0][2]==board[4] && combo_hex[0][3]==board[5]) &&
+                                !(combo_hex[0][0]==board[6] && combo_hex[0][1]==board[7]) &&
+                                !(combo_hex[0][2]==board[6] && combo_hex[0][3]==board[7]) &&
+                                !(combo_hex[0][0]==local_board[8] && combo_hex[0][1]==local_board[9]) &&
+                                !(combo_hex[0][2]==local_board[8] && combo_hex[0][3]==local_board[9]) &&
+                                
+                                !(combo_hex[1][0]==board[0] && combo_hex[1][1]==board[1]) &&
+                                !(combo_hex[1][2]==board[0] && combo_hex[1][3]==board[1]) &&
+                                !(combo_hex[1][0]==board[2] && combo_hex[1][1]==board[3]) &&
+                                !(combo_hex[1][2]==board[2] && combo_hex[1][3]==board[3]) &&
+                                !(combo_hex[1][0]==board[4] && combo_hex[1][1]==board[5]) &&
+                                !(combo_hex[1][2]==board[4] && combo_hex[1][3]==board[5]) &&
+                                !(combo_hex[1][0]==board[6] && combo_hex[1][1]==board[7]) &&
+                                !(combo_hex[1][2]==board[6] && combo_hex[1][3]==board[7]) &&
+                                !(combo_hex[1][0]==local_board[8] && combo_hex[1][1]==local_board[9]) &&
+                                !(combo_hex[1][2]==local_board[8] && combo_hex[1][3]==local_board[9])
+                                        
+                            ){
+                                        
+                                tmp_val[0]=get_value_combo(arr_hex_pos0[m],local_board);
+                                tmp_val[1]=get_value_combo(arr_hex_pos1[n],local_board);
+
+//                                 printf("pos 0 %x\n",tmp_val[0]);
+//                                 printf("pos 1 %x\n",tmp_val[1]);
+
+                                if(tmp_val[0]>tmp_val[1]){
+//                                     puts("gana 0");
+                                    tot_win_pos[0]++;
+                                }else if(tmp_val[0]<tmp_val[1]){
+//                                     puts("gana 1");
+                                    tot_win_pos[1]++;                                        
+                                }
+                                tot_combos++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        tot_combos=tot_win_pos[0]+tot_win_pos[1];
+        tot_win_pcent[0]=(tot_win_pos[0]*100)/tot_combos;
+        tot_win_pcent[1]=(tot_win_pos[1]*100)/tot_combos;
+
+        printf("SIN RIVER pos[0] %d\n",(int) tot_win_pos[0]);
+        printf("SIN RIVER pos[1] %d\n",(int) tot_win_pos[1]);
+
+        printf("tot_win_pcent[0] %.2f\n",tot_win_pcent[0]);
+        printf("tot_win_pcent[1] %.2f\n",tot_win_pcent[1]);
+        
+        printf("tot_combos %d\n",tot_combos);
+
+        
+    }
+}
--- src/get_value_combo.h
+++ src/get_value_combo.h
@@ -1,44 +1,65 @@
-int get_value_combo(int combo_hex){
-    int hero_combo[4];
-    int value_return;
-    int hero_all[14];
-    int hand_value[4];
-
-    for(int i=3;i>=0;i--){
-        hero_combo[i]=combo_hex%0x10;
-        combo_hex=combo_hex/0x10;
-    }
-    
+int get_value_combo(int combo_hex, int local_board[10]){
+   
 //     printf("hero_combo %x%x%x%x\n",hero_combo[0],hero_combo[1],hero_combo[2],hero_combo[3]);
 //     printf("board %x%x%x%x%x%x%x%x%x%x\n",board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8],board[9]);
 //     
-    hero_all[0]=board[0];
-    hero_all[1]=board[1];
-    hero_all[2]=board[2];
-    hero_all[3]=board[3];
-    hero_all[4]=board[4];
-    hero_all[5]=board[5];
-    hero_all[6]=board[6];
-    hero_all[7]=board[7];
-    hero_all[8]=board[8];
-    hero_all[9]=board[9];
-    hero_all[10]=hero_combo[0];
-    hero_all[11]=hero_combo[1];
-    hero_all[12]=hero_combo[2];
-    hero_all[13]=hero_combo[3];
-
-    hand_value[0]=is_flush(hero_all);
-    hand_value[1]=is_straight_flush(hero_all);
-    hand_value[2]=is_pair_to_quads(hero_all);
-    hand_value[3]=is_high_card(hero_all);
-
-    qsort (hand_value,4,sizeof(int),comparator);
+//     MIRAR A VER SI CONVIENE TRAERSE EL ARRAY ORDENADO DE LA OTRA FUNCIÓN O NO
+    int hero_all[14];
+    hero_all[0]=local_board[0];
+    hero_all[1]=local_board[1];
+    hero_all[2]=local_board[2];
+    hero_all[3]=local_board[3];
+    hero_all[4]=local_board[4];
+    hero_all[5]=local_board[5];
+    hero_all[6]=local_board[6];
+    hero_all[7]=local_board[7];
+    hero_all[8]=local_board[8];
+    hero_all[9]=local_board[9];
+    hero_all[10]=(combo_hex/0x1000)%0x10;
+    hero_all[11]=(combo_hex/0x100)%0x10;
+    hero_all[12]=(combo_hex/0x10)%0x10;
+    hero_all[13]=combo_hex%0x10;
+   
+//     printf("hero_all %x%x%x%x%x%x%x%x%x%x%x%x%x%x\n",hero_all[0],hero_all[1],hero_all[2],hero_all[3],hero_all[4],hero_all[5],hero_all[6],hero_all[7],hero_all[8],hero_all[9],hero_all[10],hero_all[11],hero_all[12],hero_all[13]);
+//         printf("%c%c %c%c %c%c - %c%c - %c%c - %c%c %c%c \n",
+//                arr_int_to_suit[hero_all[0]],
+//                arr_int_to_let[hero_all[1]],
+//                arr_int_to_suit[hero_all[2]],
+//                arr_int_to_let[hero_all[3]],
+//                arr_int_to_suit[hero_all[4]],
+//                arr_int_to_let[hero_all[5]],
+//                arr_int_to_suit[hero_all[6]],
+//                arr_int_to_let[hero_all[7]],
+//                arr_int_to_suit[hero_all[8]],
+//                arr_int_to_let[hero_all[9]],
+//                arr_int_to_suit[hero_all[10]],
+//                arr_int_to_let[hero_all[11]],
+//                arr_int_to_suit[hero_all[12]],
+//                arr_int_to_let[hero_all[13]] );
     
-    return hand_value[3];
+    int hand_value[3];
+    hand_value[0]=is_straight_flush(hero_all);
+    if(hand_value[0]>0x8000){
+        return hand_value[0];//dont waste time if is an straight flush
+    }
+    hand_value[1]=is_flush(hero_all);
+    hand_value[2]=is_pair_to_quads(hero_all);
+    if(hand_value[0]==0 && hand_value[1]==0 && hand_value[2]==0){
+        return is_high_card(hero_all);//dont waste time
+    }
+
+    int max_hand_value=0;
+    for(int i=0;i<3;i++){
+        if(hand_value[i]>max_hand_value){
+            max_hand_value=hand_value[i];
+        }
+    }
+    return max_hand_value;//return the highest hand value
 }
 
 int is_high_card(int hero_all[14]){
-    int i,value_combo=0,value_kicker=0;
+    char i;
+    int value_combo=0,value_kicker=0;
     for(i=0;i<14;i=i+2){
         if(hero_all[i+1]>value_combo){
             if(value_combo>value_kicker){
@@ -51,9 +72,10 @@ int is_high_card(int hero_all[14]){
     }
     return (0x10*value_combo)+value_kicker;
 }
+
 int is_straight_flush(int hero_all[14]){
-    int value_combo=0, cont=1,i,j;
-    int hero_straight[15]={0};//0x0e is the ace
+    char cont=1,i,j;
+    int value_combo=0,hero_straight[15]={0};//0x0e is the ace
     //make hero_straight running over hero_all
     for(i=0;i<14;i=i+2){
         hero_straight[hero_all[i+1]]++;
@@ -75,7 +97,7 @@ int is_straight_flush(int hero_all[14]){
     }
     if(value_combo!=0){//is an straight!
         //check if is an straight flush
-        int hero_suits_in_a_row[4]={0};
+        char hero_suits_in_a_row[4]={0};
         int flag_straight_flush=0;
         //run over 7 cards with their suits
         for(j=0;j<14;j=j+2){
@@ -100,84 +122,111 @@ int is_straight_flush(int hero_all[14]){
     return 0;
 }
 int is_pair_to_quads(int hero_all[14]){
-    int hero_all_cards[7];
-    hero_all_cards[0]=hero_all[1];
-    hero_all_cards[1]=hero_all[3];
-    hero_all_cards[2]=hero_all[5];
-    hero_all_cards[3]=hero_all[7];
-    hero_all_cards[4]=hero_all[9];
-    hero_all_cards[5]=hero_all[11];
-    hero_all_cards[6]=hero_all[13];
+    if(!(hero_all[1]==hero_all[3] || hero_all[1]==hero_all[5] || hero_all[1]==hero_all[7] || hero_all[1]==hero_all[9] || hero_all[1]==hero_all[11] || hero_all[1]==hero_all[13] ||
+    hero_all[3]==hero_all[5] || hero_all[3]==hero_all[7] || hero_all[3]==hero_all[9] || hero_all[3]==hero_all[11] || hero_all[3]==hero_all[13] ||
+    hero_all[5]==hero_all[7] || hero_all[5]==hero_all[9] || hero_all[5]==hero_all[11] || hero_all[5]==hero_all[13] ||
+    hero_all[7]==hero_all[9] || hero_all[7]==hero_all[11] || hero_all[7]==hero_all[13] ||
+    hero_all[9]==hero_all[11] || hero_all[9]==hero_all[13] ||
+    hero_all[11]==hero_all[13])){
+        return 0;//if dont have repeated cards, return now
+    }else{
 
-    qsort(hero_all_cards,7,sizeof(int),comparator);
+        char j, i, temp; 
+        char max_1=0,max_2=0,max_0=0,card_1=0,card_2=0;//i
+            
+        //fill the array with zeros
+        int hero_repeated_cards[15]={0};
 
-    int value_return=0,value_kicker=0;
-    int max_1=0,max_2=0,card_1=0,card_2=0,i;
-    int value_hand=0;
+        const int size_all_cards=7;
+        int hero_all_cards[size_all_cards];
+
+        //sort the array by the bubble method
+        hero_all_cards[0]=hero_all[1];
+        hero_all_cards[1]=hero_all[3];
+        hero_all_cards[2]=hero_all[5];
+        hero_all_cards[3]=hero_all[7];
+        hero_all_cards[4]=hero_all[9];
+        hero_all_cards[5]=hero_all[11];
+        hero_all_cards[6]=hero_all[13];
+
+        for(i=0;i<(size_all_cards-1);i++){ 
+            for(j=i+1;j<size_all_cards;j++){ 
+                if(hero_all_cards[j]<hero_all_cards[i]){ 
+                    temp=hero_all_cards[j]; 
+                    hero_all_cards[j]=hero_all_cards[i]; 
+                    hero_all_cards[i]=temp; 
+                } 
+            } 
+        } 
+        //
         
-    //fill the array with zeros
-    int hero_repeated_cards[15]={0};
-    //for upside down for count the better cards first
-    for(i=6;i>=0;i--){
-        hero_repeated_cards[hero_all_cards[i]]++;
-    }
-    for(i=0;i<15;i++){
-        if(max_1<=hero_repeated_cards[i] && hero_repeated_cards[i]>=2){
-            if(max_1>max_2){
-                max_2=max_1;
-                card_2=card_1;
-            }
-            max_1=hero_repeated_cards[i];
-            card_1=i;
-        }
-    }
-    
-    //now fill the value_hand
-    if(max_1==4){
-        //quads
+        //for upside down for count the better cards first
         for(i=6;i>=0;i--){
-            if(hero_all_cards[i]!=card_1){
-                value_kicker=hero_all_cards[i];
-                break;
+            hero_repeated_cards[hero_all_cards[i]]++;
+        }
+
+        
+        for(i=0;i<15;i++){
+            if(max_1<=hero_repeated_cards[i] && hero_repeated_cards[i]>=2){
+                if(max_1>max_2){
+                    max_2=max_1;
+                    card_2=card_1;
+                }
+                max_1=hero_repeated_cards[i];
+                card_1=i;
             }
         }
-        value_return=0x7000+card_1*0x100+value_kicker;
-    }else if(max_1==3 && (max_2==2 || max_2==3)){
-        //full house
-        value_return=0x6000+card_1*0x100+card_2*0x10;//no kicker here
-    }else if(max_1==3 && max_2==0){
-        //set
-        for(i=6;i>=0;i--){
-            if(hero_all_cards[i]!=card_1){
-                value_kicker=hero_all_cards[i];
-                break;
+        
+        char value_kicker=0;
+
+
+        //now fill the value_hand
+        if(max_1==2 && max_2==0){
+            //pair
+            for(i=6;i>=0;i--){
+                if(hero_all_cards[i]!=card_1 && hero_all_cards[i]!=card_2){
+                    value_kicker=hero_all_cards[i];
+                    break;
+                }
+            }        
+            return 0x1000+card_1*0x100+value_kicker; 
+        }else if(max_1==2 && max_2==2){
+            //2pair
+            for(i=6;i>=0;i--){
+                if(hero_all_cards[i]!=card_1 && hero_all_cards[i]!=card_2){
+                    value_kicker=hero_all_cards[i];
+                    break;
+                }
+            }        
+            return 0x2000+card_1*0x100+card_2*0x10+value_kicker;
+        }else if(max_1==3 && max_2==0){
+            //set
+            for(i=6;i>=0;i--){
+                if(hero_all_cards[i]!=card_1){
+                    value_kicker=hero_all_cards[i];
+                    break;
+                }
+            }        
+            return 0x3000+(card_1*0x100)+value_kicker;//no kicker here
+        }else if(max_1==3 && (max_2==2 || max_2==3)){
+            //full house
+            return 0x6000+card_1*0x100+card_2*0x10;//no kicker here
+        }else if(max_1==4){
+            //quads
+            for(i=6;i>=0;i--){
+                if(hero_all_cards[i]!=card_1){
+                    value_kicker=hero_all_cards[i];
+                    break;
+                }
             }
-        }        
-        value_return=0x3000+(card_1*0x100)+value_kicker;//no kicker here
-    }else if(max_1==2 && max_2==2){
-        //2pair
-        for(i=6;i>=0;i--){
-            if(hero_all_cards[i]!=card_1 && hero_all_cards[i]!=card_2){
-                value_kicker=hero_all_cards[i];
-                break;
-            }
-        }        
-        value_return=0x2000+card_1*0x100+card_2*0x10+value_kicker;
-    }else if(max_1==2 && max_2==0){
-        //pair
-        for(i=6;i>=0;i--){
-            if(hero_all_cards[i]!=card_1 && hero_all_cards[i]!=card_2){
-                value_kicker=hero_all_cards[i];
-                break;
-            }
-        }        
-        value_return=0x1000+card_1*0x100+value_kicker; 
+            return 0x7000+card_1*0x100+value_kicker;
+        }
+        return 0;   
     }
-    return value_return;    
 }
 
 int is_flush(int hero_all[14]){
-    int i,j,value_combo=0, hero_suits[4]={0};
+    char i,j,value_combo=0, hero_suits[4]={0};
     for(i=0;i<14;i=i+2){
         hero_suits[hero_all[i]]++;
     }

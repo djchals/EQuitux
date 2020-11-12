ac00f806eeb003e44c316512cb7d3647fa4f3d53
Carlos H <djtv93@yahoo.es>
2020-11-12 02:07:38 +0100

    fixed a bug when calculating toomuch hands preflop

--- src/threadcalculate.cpp
+++ src/threadcalculate.cpp
@@ -39,20 +39,19 @@ void ThreadCalculate::run(){
     ){
         //preflop
         //in the future put a button for stop and put i higher, maybe i=10000
-        for(i=0;i<100000;i++){
+        for(i=0;i<60000;i++){
             //here put the same number defined in NUM_HANDS_PREFLOP
             create_preflop(100);
-
             for(i_player=0;i_player<this->num_players;++i_player){
                 if(range_pkr[i_player]){
                     //the result of this function will be saved in arr_hex_return
                     conv_range_pkr_to_hex(range_pkr[i_player],i_player);
+
                     if(long_hex_pos[i_player]>0){
                         calculate_EQ(i_player);
                     }
                 }
                 if(this->flag_stop_process){
-                qDebug() <<"parado 1";
                     break;
                 }
             }
@@ -62,7 +61,6 @@ void ThreadCalculate::run(){
                 emit show_win_pcent(j,tot_win_pcent[j]);
             }
             if(this->flag_stop_process){
-                qDebug() <<"parado 2";
                 break;
             }
 //            for(j=0;j<num_players;j++){
